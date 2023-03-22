aa7a86e98733180e0434a84b36681dca29a2b417
ernestsmotlak <ernestsmotlak@gmail.com>
2023-03-22 10:57:34 +0100

    nothing

--- src/Jugo.java
+++ src/Jugo.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class Jugo {
     public Jugo() {
-        String bosnian_tweet = "Igrali smo veoma dobro, pobijedili, ali smo zaista propustili mnogo prilika.";
+        String bosnian_tweet = "Kako je lepo kada su ulice pune ljudi, smeha, gužve i života. Nadam se da ćemo se uskoro vratiti tom normalnom stanju. #optimizam #život #beograd";
+
+        //"Igrali smo veoma dobro, pobijedili, ali smo zaista propustili mnogo prilika."
         String[] cleaned_tweet = changeTweet(bosnian_tweet);
         ArrayList<String> jugoNegative = negativeCorpus();
         ArrayList<String> jugoPositive = positiveCorpus();
