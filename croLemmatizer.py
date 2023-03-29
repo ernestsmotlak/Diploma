4dcdba7da5642b23df6d82474dea5681c52d6a1e
ernestsmotlak <ernestsmotlak@gmail.com>
2023-03-29 14:37:54 +0200

    Just testing

--- croLemmatizer.py
+++ croLemmatizer.py
@@ -3,7 +3,7 @@ import spacy
 nlp = spacy.load("hr_core_news_md")
 
 #text = "Ovo je primjer rečenice za lematizaciju."
-text = "kako je lepo kada su ulice pune ljudi smeha gužve i života nadam se da ćemo se uskoro vratiti tom normalnom stanju optimizam život beograd"
+text = "Kako je lepo kada su ulice pune ljudi smeha gužve i života nadam se da ćemo se uskoro vratiti tom normalnom stanju optimizam život beograd"
 # text1 = "Kako je lepo kada su ulice pune ljudi, smeha, gužve i života. Nadam se da ćemo se uskoro vratiti tom normalnom stanju. #optimizam #život #beograd"
 # text4 = "Zašto se nešto ne može platiti karticom ako se može kupiti na rate? Ovo je čista diskriminacija ljudi koji nemaju novca da plate sve odjednom. #nepravda #bankelopovi"
 # text2 = "nekoliko rečenica na srpskom jeziku"
