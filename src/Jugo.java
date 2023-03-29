6128070ca5cb8009ae5a0c90911f4d340ce9b64b
ernestsmotlak <ernestsmotlak@gmail.com>
2023-03-29 15:34:41 +0200

    Made a python lemmatizator and connected it with a java program for jugo languages

--- src/Jugo.java
+++ src/Jugo.java
@@ -2,47 +2,121 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class Jugo {
     public Jugo() {
+        //String inputString = "Kako je lepo kada su ulice pune ljudi smeha gužve i života nadam se da ćemo se uskoro vratiti tom normalnom stanju optimizam život beograd koji kurac";
+        String inputString = "Kako je plaho lepo kada su ulice definitivno pune ljudi, smeha, gužve i života. Nadam se da ćemo se uskoro vratiti tom stvarno normalnom stanju. #optimizam #život #beograd";
+        String outputString = executePythonScript(inputString);
+
         String bosnian_tweet = "Kako je plaho lepo kada su ulice definitivno pune ljudi, smeha, gužve i života. Nadam se da ćemo se uskoro vratiti tom stvarno normalnom stanju. #optimizam #život #beograd";
 
-        // "Igrali smo veoma dobro, pobijedili, ali smo zaista propustili mnogo
-        // prilika."
-        String[] cleaned_tweet = changeTweet(bosnian_tweet);
+        // moja koda
+
+        String[] cleaned_tweet = changeTweet(inputString);
         ArrayList<String> jugoNegative = negativeCorpus();
         ArrayList<String> jugoPositive = positiveCorpus();
         ArrayList<String> anaWords = anaCorpus();
 
         float[] result = difference(cleaned_tweet, jugoPositive, jugoNegative);
+        int number_of_anaWords = find_anaWords(cleaned_tweet, anaWords);
 
+        System.out.println();
         System.out.println("Tweet: ");
+        System.out.println(inputString);
         System.out.println("_______________________");
-        System.out.println(bosnian_tweet);
         System.out.println();
         System.out.println("Processed tweet: ");
-        System.out.println("_______________________");
         printProcessedTweet(cleaned_tweet);
         System.out.println();
+        System.out.println("_______________________");
         System.out.println();
         printPositiveWords(cleaned_tweet, jugoPositive);
+        System.out.println("_______________________");
         System.out.println();
         printNegativeWords(cleaned_tweet, jugoNegative);
+        System.out.println("_______________________");
+        System.out.println();
+        System.out.println("Basic sentiment : " + sentimentBasic(result[0], result[1], result[2]) + "");
+        System.out.println();
+        System.out.println("_______________________");
         System.out.println();
 
-        System.out.println("Sentiment of the tweet is " + sentimentBasic(result[0], result[1], result[2]) + "");
-        System.out.println("_______________________");
-
-        int number_of_anaWords = find_anaWords(cleaned_tweet, anaWords);
-        System.out.println("Number of AnA words is: " + number_of_anaWords);
-        System.out.println("_______________________");
 
         System.out.println("AnA words:");
         print_anaWords(cleaned_tweet, anaWords);
-
         System.out.println("_______________________");
+        System.out.println();
+        System.out.println("Number of AnA words is: " + number_of_anaWords);
+        System.out.println("_______________________");
+        System.out.println();
         System.out.println("Advanced sentiment is: " + sentimentAdvanced(result, number_of_anaWords));
+        System.out.println("_______________________");
 
+        // moja koda
+        // System.out.println(outputString);
+
+        String[] words = outputString.split(" "); //turn String to array of strings
+        System.out.println();
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(" " + words[i]);
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < cleaned_tweet.length; i++) {
+            System.out.print(" " + cleaned_tweet[i]);
+        }
+
+        System.out.println();
+
+    }
+
+    public static String executePythonScript(String inputString) {
+        String outputString = "";
+
+        try {
+            // Build the command to run the Python script
+            List<String> commands = new ArrayList<String>();
+            commands.add("python3");
+            commands.add("/Users/ernestsmotlak/Desktop/Famnit/java_python_java/py.py");
+
+            // Start the process to run the Python script
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            Process p = pb.start();
+
+            // Get the input stream of the Python process
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            // Send the input string to the Python process
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+            out.write(inputString);
+            out.newLine();
+            out.flush();
+
+            // Read the output of the Python process
+            String line;
+            while ((line = in.readLine()) != null) {
+                outputString += line;
+            }
+
+            // Close the input and output streams
+            in.close();
+            out.close();
+
+            // Wait for the Python process to complete
+            int exitCode = p.waitFor();
+            System.out.println("Python script execution completed with exit code " + exitCode);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return outputString;
     }
 
     static String[] changeTweet(String str) {
@@ -137,8 +211,7 @@ public class Jugo {
         return 1.0;
     }
 
-    public static double Result(String[] words, ArrayList<String> positiveAL, ArrayList<String> negativeAL,
-            double posWords, double negWords) {
+    public static double Result(String[] words, ArrayList<String> positiveAL, ArrayList<String> negativeAL, double posWords, double negWords) {
         for (String word : words) {
             for (int j = 0; j < negativeAL.toArray().length; j++) {
                 if (findSimilarity(negativeAL.get(j), word) >= 0.8) {
@@ -160,8 +233,7 @@ public class Jugo {
         return (posWords - negWords) / words.length;
     }
 
-    public static float[] difference(String[] words, ArrayList<String> positiveLexicon,
-            ArrayList<String> negativeLexicon) {
+    public static float[] difference(String[] words, ArrayList<String> positiveLexicon, ArrayList<String> negativeLexicon) {
         int posWords = 0;
         int negWords = 0;
         for (String word : words) {
@@ -180,7 +252,7 @@ public class Jugo {
         for (String word : words) {
             if (anaCorpus.contains(word)) {
                 number_of_anaWords++;
-            } 
+            }
         }
         return number_of_anaWords;
     }
